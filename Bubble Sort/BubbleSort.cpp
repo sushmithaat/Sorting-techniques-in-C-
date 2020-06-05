9603a792ddf38ec9bde3504b897edcabffcf092f
Sushmitha A T <48562432+sushmithaat@users.noreply.github.com>
2020-06-05 22:22:01 +0530

    Create BubbleSort.cpp

---
+++ Bubble Sort/BubbleSort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,a[100],temp;
+
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<"\t";
+    }
+
+    return 0;
+}
